bdbb0a0756d0985d274a5794821c8cefab3b8bb0
luis1341 <98926603+luis1341@users.noreply.github.com>
2022-02-03 01:25:04 +0000

    Create main.c

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int a = 100;
+    int b = 50;
+    int soma;
+    printf("Hello world!\n");
+    soma = a + b;
+    printf("Soma = %d\n",soma);
+    return 0;
+}
